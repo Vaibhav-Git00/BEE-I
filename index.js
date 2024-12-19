d1afe9117df4fa92f313f5bb9721ff0a672f483e
Vaibhav <itsvaibhav002@gmail.com>
2024-12-19 17:57:45 +0530

    update

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
-var express=require("express")
-var bodyParser=require("body-parser")
-var mongoose=require("mongoose")
+// Updated HTML
+// Make sure the HTML reflects the additional inputs for 'Branch' and 'State'.
 
-const app=express()
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-app.use(bodyParser.json())
-app.use(express.static('public'))
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
-    extended:true
-}))
+    extended: true
+}));
 
-mongoose.connect('mongodb://localhost:27017/Database')
-var db=mongoose.connection
-db.on('error',()=> console.log("Error in Connecting to Database"))
-db.once('open',()=> console.log("Connected to Database"))
+mongoose.connect('mongodb://localhost:27017/Database');
+var db = mongoose.connection;
+db.on('error', () => console.log("Error in Connecting to Database"));
+db.once('open', () => console.log("Connected to Database"));
 
-app.post("/sign_up",(req,res) => {
-    var name= req.body.name
-    var age=req.body.age
-    var email=req.body.email
-    var phno=req.body.phno
-    var gender=req.body.gender
-    var password=req.body.password
+app.post("/sign_up", (req, res) => {
+    var name = req.body.name;
+    var age = req.body.age;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var gender = req.body.gender;
+    var branch = req.body.branch; // Added Branch
+    var state = req.body.state;   // Added State
+    var password = req.body.password;
 
-    var data={
-        "name":name,
-        "age":age,
-        "email":email,
-        "phno":phno,
-        "gender":gender,
-        "password":password
-    }
-    db.collection('users').insertOne(data,(err,collection) => {
-        if(err){
+    var data = {
+        "name": name,
+        "age": age,
+        "email": email,
+        "phno": phno,
+        "gender": gender,
+        "branch": branch, // Include Branch in the database
+        "state": state,   // Include State in the database
+        "password": password
+    };
+
+    db.collection('users').insertOne(data, (err, collection) => {
+        if (err) {
             throw err;
         }
-        console.log("Record Inserted Succesfully")
-    })
-    return res.redirect('signup_successful.html')
-})
+        console.log("Record Inserted Successfully");
+    });
+    return res.redirect('signup_successful.html');
+});
 
-app.get("/",(req,res) => {
+app.get("/", (req, res) => {
     res.set({
-        "Allow-acces-Allow-Origin":'*'
-    })
-    return res.redirect('index.html')
+        "Allow-access-Allow-Origin": '*'
+    });
+    return res.redirect('index.html');
 }).listen(3000);
 
-console.log("Listening on port 3000")
+console.log("Listening on port 3000");
